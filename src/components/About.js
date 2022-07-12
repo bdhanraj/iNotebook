7ed89d679fe7f44c2c2fdd3437108ba4f3f018a8
Dhanraj Bhati <dhanrajbhati13042003@gmail.com>
2022-07-12 10:52:09 +0530

    iNotebook first commit

---
+++ src/components/About.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const About = () => { 
+    return (
+        <div className="container text-center">
+            <div className="box my-2">
+                <h2>iNotebook</h2> 
+                Your notebook on cloud.  
+                <br />
+                We dont share your notes with anyone else 
+           </div>
+        </div>
+    )
+}
+
+export default About
